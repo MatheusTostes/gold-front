54444eba962080491f310f6c0ca2469dd53234f3
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 11:31:05 -0300

    change back url again

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:8081/';
-const baseURL = 'https://2a87-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io';
+const baseURL = 'http://2a87-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io';
 
 const api = axios.create({
   baseURL,
