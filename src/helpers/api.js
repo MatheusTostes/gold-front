0f74244c358be7f5d9f8fc1c2bfdff230935854c
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 11:51:03 -0300

    change urls headers

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 const baseURL = 'https://2a87-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io/';
 
 axios.defaults.withCredentials = true
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'https://gold-front.vercel.app';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://gold-front.vercel.app';
+axios.defaults.headers.put['Access-Control-Allow-Origin'] = 'https://gold-front.vercel.app';
+axios.defaults.headers.delete['Access-Control-Allow-Origin'] = 'https://gold-front.vercel.app';
 
 const api = axios.create({
   baseURL,
