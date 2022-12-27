e3c27891085efdd6ad3828b330e498a688d26376
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 12:14:56 -0300

    fix

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 const baseURL = 'https://e1ac-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io';
 
 // axios.defaults.withCredentials = true
-// axios.defaults.headers.get['Access-Control-Allow-Origin'] = baseURL;
-// axios.defaults.headers.post['Access-Control-Allow-Origin'] = baseURL;
-// axios.defaults.headers.put['Access-Control-Allow-Origin'] = baseURL;
-// axios.defaults.headers.delete['Access-Control-Allow-Origin'] = baseURL;
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
 
 const api = axios.create({
   baseURL,
-  // withCredentials: false,
+  withCredentials: false,
   timeout: 10000,
 });
 
