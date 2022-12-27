37f680de9f9caf0e5317d4357c5634774d5822a0
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 11:43:26 -0300

    change credentials

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,7 +5,7 @@ const baseURL = 'https://2a87-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io/';
 
 const api = axios.create({
   baseURL,
-  withCredentials: false,
+  withCredentials: true,
   timeout: 10000,
 });
 
