d952cf374f0808019a7eb40f7809651f5c2b61c4
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 11:49:15 -0300

    change axios headers

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 const baseURL = 'https://2a87-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io/';
 
 axios.defaults.withCredentials = true
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
 
 const api = axios.create({
   baseURL,
