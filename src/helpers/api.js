ea5361a2fb238a014f41877c2bd361fe45e716d9
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-27 12:33:35 -0300

    fix cors?

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 const baseURL = 'https://e1ac-2804-56c-a403-4700-7129-2eb1-4958-d8d0.ngrok.io';
 
 // axios.defaults.withCredentials = true
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = true;
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get["ngrok-skip-browser-warning"] = true;
 
 const api = axios.create({
   baseURL,
